06855cb5d23bf3261abddbfe56762a128bdfea90
Dhiraj KC <62998336+DKC195@users.noreply.github.com>
2025-02-15 00:10:17 +0545

    Create Q01.swift

---
+++ Swift/Assignment01/Q01.swift
@@ -0,0 +1,18 @@
+// WAP to declare variable of all Data Types, read the values and display them.
+var test: Int = 10
+print("Int ", test)
+
+var test1: Float = 20.0
+print("Float ", test1)
+
+var test2: Double = 30.0
+print("Double ", test2)
+
+var test3: String = "Hello"
+print("String ", test3)
+
+var test4: Character = "A"
+print("Character ", test4)
+
+var test5: Bool = true
+print("Bool ", test5)
