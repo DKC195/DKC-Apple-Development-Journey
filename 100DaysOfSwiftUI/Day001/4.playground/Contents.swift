3f306443a6932b198db9db4484264043e35b665b
Dhiraj KC <62998336+DKC195@users.noreply.github.com>
2025-04-14 11:51:01 +0545

    Day 001

---
+++ 100DaysOfSwiftUI/Day001/4.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+//character = "Eloise"
+//character = "Francesca"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Micheal Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many reads must a man walk down?"
