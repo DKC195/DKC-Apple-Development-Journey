4eb5393ff0bb79545d4647788146bad53c2d91f4
Dhiraj KC <62998336+DKC195@users.noreply.github.com>
2025-04-16 00:35:06 +0545

    Day 002

---
+++ 100DaysOfSwiftUI/Day002/2.playground/Contents.swift
@@ -0,0 +1,27 @@
+import Cocoa
+
+let firstPart = "Hello, "
+let lastPart = "world!"
+let greeting = firstPart + lastPart
+
+let people = "Haters"
+let action = "hate"
+let lyrics = people + "gonna" + action
+print(lyrics)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let name = "Dhiraj KC"
+let age = 22
+let message = "Hello, my name is \(name) ans I'm \(age) years old."
+
+print(message)
+
+let number = 11
+//let missionMessage = "Apollo " + String(number) + " landed on the moon."
+let missionMessage = "Apollo \number) landed on the moon."
+
+print("5 x 5 is \(5*5)")
+
---
+++ 100DaysOfSwiftUI/Day002/C1.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+let celsius = 32.0
+
+let fahrenheit = (celsius * 9) / 5 + 32
+
+print("\(celsius)°C => \(fahrenheit)°F")
+
